65742d1ba739a4114c3b60ee0f68f07aa81a958b
damola adebayo <adebayo10k@gmail.com>
2022-07-21 19:58:15 +0100

    move related functions to separate included files

---
+++ includes/run-quiz.inc.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+# iterate over num_range_arr numbers array to select questions in the global current quiz
+function run_quiz()
+{
+    clear # clear console before showing new quiz information
+
+    # display:
+    # quiz name (unique)
+    # quiz size (number of questions)
+    # quiz play sequence (ordered or shuffled)
+    # quiz content (a preview)
+    # quiz instructions 
+
+	# display quiz theme (or name) and instructions
+    echo "quiz theme (or name):"
+    echo -e "${quiz_category_string}"
+    echo
+
+    # quiz size (number of questions)
+    echo "$quiz_length questions"
+    echo
+
+    # quiz play sequence (ordered or shuffled)
+    echo "quiz questions sequence (ordered or shuffled):"
+	echo -e "$quiz_play_sequence_default_string"
+	echo
+
+    # quiz content (a preview)
+    echo "quiz_english_phrases_string:"
+	echo -e "$quiz_english_phrases_string"
+	echo && echo
+
+    echo "quiz_yoruba_phrases_string:"
+	echo -e "$quiz_yoruba_phrases_string"
+	echo && echo
+
+    echo "Press ENTER to continue..." && read # user acknowledges info
+    clear
+
+    # quiz instructions 
+	for line in "${quiz_instructions_array[@]}"
+	do
+		echo -e "$line"
+	done
+
+	echo "Press ENTER to continue..." && read # user acknowledges info
+	
+	# create a number sequence to 'pilot' the quiz order
+	if [[ "$quiz_play_sequence_default_string" = 'shuffled' ]]
+	then
+		make_shuffled_num_range 0 "$(( ${#current_english_phrases_list[@]} - 1 ))"
+	elif [[ "$quiz_play_sequence_default_string" = 'ordered' ]]
+	then
+		make_ordered_num_range 0 "$(( ${#current_english_phrases_list[@]} - 1 ))"
+	else
+		## exit with error code and message
+        msg="quiz play sequence not set. Exiting now..."
+		lib10k_exit_with_error "$E_UNEXPECTED_BRANCH_ENTERED" "$msg"
+	fi
+
+	# initialise before quiz starts
+	num_of_responses_showing=0 
+	is_first_quiz_question='true'
+
+	for elem in ${num_range_arr[@]}
+	do		
+		# always clear screen before first quiz question
+		if [ "$is_first_quiz_question" = 'true' ]
+		then
+			clear
+			is_first_quiz_question='false'
+		fi
+
+		# then only clear when num_of_responses_showing >= num_of_responses_to_display
+		if [ "$num_of_responses_showing" -ge "$num_of_responses_to_display" ]
+		then
+			clear
+			num_of_responses_showing=0 # reset
+		fi
+		
+		echo && echo && echo # for positioning and display of next console output	only	
+
+		# handle quiz question serve method based on the quiz_type_string
+		if [[ "$quiz_type_string" = 'vocabulary' ]]
+		then
+			serve_vocabulary_question "$elem"
+		elif [[ "$quiz_type_string" = 'oral' ]]
+		then
+			serve_oral_question "$elem"
+		else
+			## exit with error code and message
+            msg="quiz type not set. Exiting now..."
+            lib10k_exit_with_error "$E_UNEXPECTED_BRANCH_ENTERED" "$msg"
+		fi
+
+		num_of_responses_showing=$((num_of_responses_showing + 1))
+		read	# wait for user to acknowledge answer
+
+	done
+}
+
+##############################
+# populates a globally accessible array with shuffled integer values
+function make_shuffled_num_range() {
+
+	lower_limit=$1 # array start index
+	upper_limit=$2 # (array size - 1) is passed in
+
+	num_range_arr=()	# reset this global variable
+	for index in "$(shuf -i "$1"-"$2")"
+	do
+		num_range_arr+=("${index}") # append an indexed array of shuffled numbers
+	done
+}
+##############################
+# populates a globally accessible array with ordered, sequenced integer values
+function make_ordered_num_range() {
+	lower_limit=$1 # array start index
+	upper_limit=$2 # (array size - 1) is passed in
+
+	num_range_arr=()	# reset this global variable
+	for index in "$(seq "$lower_limit" "$upper_limit")"
+	do
+		num_range_arr+=("${index}") # append an indexed array of sequenced numbers
+	done
+}
+##############################
+# vocabulary questions wait for user to respond before displaying answer
+function serve_vocabulary_question() {
+	num=$1
+
+	eng_word="${current_english_phrases_list[$num]}"
+    # -e because some english phrases include Yoruba names
+	echo -e "		$eng_word" && echo
+    echo "1. Type your answer (Optional)." 
+    echo "2. Press ENTER to see translation..." && read # wait for user to answer
+	#read	# wait for user to answer
+
+	# if translation is a colon separated list, print a listing
+	translatedString="${current_yoruba_translations[$eng_word]}"
+	echo "$translatedString" | grep -q ':'
+	isList=$?
+	if [ $isList -eq 0 ]	# 0 means colon delimited translation lines/list was found
+	then
+		enum_list "$translatedString"
+	else
+		echo -e "		${translatedString}"
+	fi
+}
+
+# oral questions just serve a series of phrases, with no specific answer given
+# if quiz type is oral, just iterate over as a list
+function serve_oral_question() {
+	num=$1
+	# if question is a colon separated lines, print a listing
+	yoruba_oral_question="${current_yoruba_oral_questions[$num]}"
+	echo "$yoruba_oral_question" | grep -q ':'
+	isList=$?
+	if [ $isList -eq 0 ]	# 0 means colon delimited oral question lines are present
+	then
+		enum_list "$yoruba_oral_question"		
+	else
+		echo -e "		${yoruba_oral_question}"
+	fi
+}
+##############################
+function enum_list() {
+	list=$1 #
+	while [ ${#list} -gt 0 ]
+	do
+		item=${list%%':'*}
+		list=${list#"${item}:"} 
+		echo -e "		$item" # 
+	done
+}
+
+##############################
